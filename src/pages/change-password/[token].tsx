8b2d1a6c1e48c91b60178dec122e26a6d0c884e5
Farid <faridforward222@gmail.com>
2022-07-24 21:18:43 +0430

    added change password mutation

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,9 +1,36 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import InputField from '../../components/InputField';
+import Wrapper from '../../components/Wrapper';
 
 const ChangePassword: NextPage<{ token: string }> = ({}) => {
   const { token } = useRouter().query;
 
-  return <div>token is : {token}</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: '' }}
+        onSubmit={async (values, { setErrors }) => {}}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Box mt={4}>
+              <InputField
+                name="newPassword"
+                placeholder="New Password"
+                label="New Password"
+                type="password"
+              />
+            </Box>
+            <Button mt={4} isLoading={isSubmitting} type="submit" color="teal">
+              Change Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 export default ChangePassword;
